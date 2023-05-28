51af65aa461e0070d6dde3903e6d9ac5647b181a
tushit03 <tushit.shukla03@gmail.com>
2023-05-28 22:24:39 +0530

    telegramWeatherBot

--- api/webhook.js
+++ api/webhook.js
@@ -1,44 +1,56 @@
-// https://github.com/yagop/node-telegram-bot-api/issues/319#issuecomment-324963294
-// Fixes an error with Promise cancellation
+
 process.env.NTBA_FIX_319 = 'test';
 
-// Require our Telegram helper package
+const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 
-// Export as an asynchronous function
-// We'll wait until we've responded to the user
+TOKEN = ""
+
 module.exports = async (request, response) => {
     try {
-        // Create our new bot handler with the token
-        // that the Botfather gave us
-        // Use an environment variable so we don't expose it in our code
-        const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+       
+        const bot = new TelegramBot("6228337921:AAH2f5XJnJoPNgmid1pu2Hl6fbtJHRUN1rQ");
+        
+          
 
-        // Retrieve the POST request body that gets sent from Telegram
         const { body } = request;
-
-        // Ensure that this is a message being sent
         if (body.message) {
-            // Retrieve the ID for this chat
-            // and the text that the user sent
+            
             const { chat: { id }, text } = body.message;
+            const response = await axios.get('http://api.weatherapi.com/v1/current.json', {
+            params: {
+              key: "3f3331598a8d4dd1b6a191102232705",
+              q: `${text}`,
+              aqi: 'no'
+            }
 
-            // Create a message to send back
-            // We can use Markdown inside this
-            const message = `✅ Thanks for your message: *"${text}"*\nHave a great day! 👋🏻`;
+          });
+          const markdownResponse = `
+          *Weather Forecast 🛰️*
+          _Location:_ ${text }📍
+          _Temperature:_ **${response.data.current.temp_c}°C**🌡️
+          _Condition:_  ${response.data.current.condition.text}
 
-            // Send our new message back in Markdown
-            await bot.sendMessage(id, message, {parse_mode: 'Markdown'});
+          *Additional Details*
+          - Wind Speed: ${response.data.current.wind_kph}Km/h 💨
+          - Humidity: ${response.data.current.humidity}%💧    
+          - Visibility: ${response.data.current.vis_km} km 👀
+
+          *Location Information*
+          - Latitude: ${response.data.location.lat}🌍
+          - Longitude: ${response.data.location.lon}🌍
+          - Timezone: ${response.data.location.tz_id}🌐
+          - Current Local Time: ${response.data.location.localtime}⏰
+          `;
+         
+          await bot.sendMessage(id, markdownResponse, {parse_mode: 'Markdown'});
         }
     }
     catch(error) {
-        // If there was an error sending our message then we 
-        // can log it into the Vercel console
+   
         console.error('Error sending message');
         console.log(error.toString());
     }
-    
-    // Acknowledge the message with Telegram
-    // by sending a 200 HTTP status code
+   
     response.send('OK');
 };
